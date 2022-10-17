8166fa4ccdabc271d75ea93acecaf4b0f8da8f82
merykul <mariakulv@gmail.com>
2022-10-17 12:03:10 +0300

    first version of 3 task

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 import lesson6tasks.Circle;
+import lesson6tasks.Dog;
 
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangleFirst = new Rectangle(12,23);
         Circle circleFirst = new Circle(12,3.14);
+        Dog dogFirst = new Dog("Retriever",2,"Doggy");
 
         System.out.println(rectangleFirst.Area(12,23));
         System.out.println(rectangleFirst.Perimeter(12,23));
 
         System.out.println(circleFirst.CircleArea(12,3.14));
         System.out.println(circleFirst.CirclePerimeter(12,3.14));
+
+        System.out.println(dogFirst.Sit("Retriever", 2,"Doggy"));
+        System.out.println(dogFirst.Eat("Retriever", 2,"Doggy"));
     }
 }
---
+++ src/lesson6tasks/Dog.java
@@ -0,0 +1,13 @@
+package lesson6tasks;
+
+public class Dog {
+    public Dog (String breed, int age, String name) {
+
+    }
+    public String Sit (String breed, int age, String name) {
+        return "Dog is sitting";
+    }
+    public String Eat (String breed, int age, String name) {
+        return "Dog is eating";
+    }
+}
