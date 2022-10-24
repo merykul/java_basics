2120be83dd8ff2263ca8705c23c90a969763d3d1
Mariia <114490803+merykul@users.noreply.github.com>
2022-10-24 11:03:27 +0300

    Update src/lesson7/Shape.java
    Co-authored-by: artembelmeha <41549134+artembelmeha@users.noreply.github.com>

--- src/lesson7/Shape.java
+++ src/lesson7/Shape.java
@@ -1,27 +1,8 @@
 package lesson7;
 
 public abstract class Shape {
-    protected double width;
-    protected double high;
-    public Shape() {
-
-    }
-    public Shape(double width, double high) {
-    }
-    public Shape(double high) {
-    }
-
-    public void setHigh(double high) {
-        this.high = high;
-    }
-
-    public void setWidth(double width) {
-        this.width = width;
-    }
-
     public abstract double getPerimeter ();
     public abstract double getArea ();
-
     public abstract String dataAboutShape();
 }
 
