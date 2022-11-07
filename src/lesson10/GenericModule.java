c2ae2c8f137af8cde5934fb9b65db6a19f28b30b
merykul <mariakulv@gmail.com>
2022-11-08 00:02:21 +0300

    in progress

--- src/lesson10/GenericModule.java
+++ src/lesson10/GenericModule.java
@@ -1,6 +1,64 @@
 package lesson10;
 
+import java.time.Year;
+
 public class GenericModule {
+
+    // 1 зд, можливо створити Factory
+    static class SaveNew<T> {
+        private T data;
+        public SaveNew(T input){
+            this.data = input;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+    }
+
+    static class SaveValueWithMinAndMax<Integer extends Comparable<Integer>> {
+        private Integer value;
+        private Integer min;
+        private Integer max;
+
+        public SaveValueWithMinAndMax() {
+        }
+
+        public Integer setMax(Integer v1, Integer v2, Integer v3) {
+            if (v1.compareTo(v2) > 0 && v1.compareTo(v3) > 0) {
+                this.max = v1;
+            } else if (v2.compareTo(v1) > 0 && v1.compareTo(v3) > 0) {
+                this.max = v2;
+            } else this.max = v3;
+            return this.max;
+        }
+
+        public Integer setMin(Integer v1, Integer v2, Integer v3) {
+            if (v1.compareTo(v2) < 0 && v1.compareTo(v3) < 0) {
+                this.min = v1;
+            } else if (v2.compareTo(v1) < 0 && v1.compareTo(v3) < 0) {
+                this.min = v2;
+            } else this.min = v3;
+            return this.min;
+        }
+
+        public Integer setValue(Integer v1, Integer v2, Integer v3) {
+            if (v1.compareTo(v2) > 0 && v1.compareTo(v3) < 0) {
+                this.value = v1;
+            } else if (v2.compareTo(v1) > 0 && v1.compareTo(v3) < 0) {
+                this.value = v2;
+            } else this.value = v3;
+            return this.value;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
+    }
 
     @FunctionalInterface
     interface Define<F, T> {
--- src/lesson10/Main.java
+++ src/lesson10/Main.java
@@ -3,6 +3,16 @@ package lesson10;
 public class Main {
     public static void main(String[] args) {
 
+        GenericModule.SaveNew saveNew = new GenericModule.SaveNew<>(28);
+        System.out.println(saveNew.getData());
+
+        GenericModule.SaveValueWithMinAndMax saveValue = new GenericModule.SaveValueWithMinAndMax<>(); // 2-nd task
+        System.out.println(saveValue.setMax(12, 45, 56));
+        System.out.println(saveValue.setMin(12, 45, 56));
+        System.out.println(saveValue.setValue(12, 45, 56));
+        System.out.println(saveValue.getValue());
+
+
         GenericModule.Define<Integer, String> definedConverter = s -> s.toString();            // 3-rd task
         System.out.println(definedConverter.convert(154));
 
