1d32e5280fe6102e9654fb7503e4c51cd3fe3c9f
merykul <mariakulv@gmail.com>
2022-11-23 22:38:51 +0300

    adjustment

--- src/lesson16/Main.java
+++ src/lesson16/Main.java
@@ -2,6 +2,8 @@ package lesson16;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+
+// 2-nd task
         Thread firstThread = new Numbers("#1", 200);
         Thread secondThread = new Numbers("#2",200);
         Thread thirdThread = new Numbers("#3",250);
