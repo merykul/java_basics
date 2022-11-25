7f67f0e78b53bc6e509fbf7a4de3165fa43f74b1
merykul <mariakulv@gmail.com>
2022-11-25 11:55:30 +0300

    3-rd task of 16-nt homework is done

--- src/lesson16/EvenNumbers.java
+++ src/lesson16/EvenNumbers.java
@@ -8,6 +8,9 @@ public class EvenNumbers extends Thread{
     }
     @Override
     public void run() {
+        printEvenNumbers();
+    }
+    private synchronized void printEvenNumbers() {
         for (int i = 0; i <= 9; i++) {
             if (i % 2 == 0) {
                 System.out.println(i + " -> " + getThreadName());
--- src/lesson16/OddNumbers.java
+++ src/lesson16/OddNumbers.java
@@ -7,6 +7,9 @@ public class OddNumbers extends Thread{
     }
     @Override
     public void run() {
+        printOddNumbers();
+    }
+    private synchronized void printOddNumbers() {
         for (int i = 0; i <= 9; i++) {
             if (i % 2 != 0) {
                 System.out.println(i + " -> " + getThreadName());
